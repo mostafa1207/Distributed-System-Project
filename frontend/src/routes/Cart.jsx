837b76cb98d4283b2f5253f9ffb7e325d91b92c5
Ahmed Adel <73443649+ahmadel1@users.noreply.github.com>
2024-04-29 20:48:58 +0300

    modifying cart

--- frontend/src/routes/Cart.jsx
+++ frontend/src/routes/Cart.jsx
@@ -1,10 +1,15 @@
+import CartItem from "../Components/CartItem";
+import Summary from "../Components/summary";
+import React from 'react';
+import './cart.css';
 
 export default function Cart() {
 
 
     return (
-        <>
-            <div className="main-container">Cart</div>
-        </>
+        <div className="cart">
+            <CartItem></CartItem>
+            <Summary></Summary>
+        </div>
     );
 }
