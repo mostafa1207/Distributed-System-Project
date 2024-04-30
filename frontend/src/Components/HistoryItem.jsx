ce5fc92ba0eba31754ea6c5aedd6d869784e77e1
FadyAdel10 <fadya4265@gmail.com>
2024-05-01 01:42:27 +0300

    PurchaseHistory

---
+++ frontend/src/Components/HistoryItem.jsx
@@ -0,0 +1,21 @@
+import "./HistoryItem.css"
+export const HistoryItem = ({
+    Date,Address,Amount
+}
+) =>{
+    return (
+        <>
+           {/* <div class="rounded-rectangle"> */}
+            {/* <h4>Purchased history</h4> */}
+            {/* <div class="solid-line"></div> */}
+            <div class="solid-line"></div>
+            <div class="Item_Info">
+            <paragraph>Date: {Date}</paragraph>
+            <paragraph>Address: {Address}</paragraph>
+            <paragraph>Total Amount: {Amount}</paragraph>
+            </div>
+            <h5></h5>
+        {/* </div> */}
+        </>
+    );
+}
--- frontend/src/routes/Profile.jsx
+++ frontend/src/routes/Profile.jsx
@@ -1,12 +1,19 @@
-import { useOutletContext } from "react-router-dom";
+import { HistoryItem } from "../Components/HistoryItem";
 
 export default function Profile(props) {
 
-    const userType = useOutletContext();
 
     return (
         <>
-            <div className="main-container">{userType} Profile</div>
+            <div className="main-container">{props.userType} Profile</div>
+            <div class="rounded-rectangle">
+            <h4>Purchased history</h4>
+            {/* <div class="solid-line"></div> */}
+                <HistoryItem Date="24/2/2002" Address="ElNozha" Amount="2000"></HistoryItem>
+                <HistoryItem Date="24/2/2002" Address="ElNozha" Amount="2000"></HistoryItem>
+                <HistoryItem Date="24/2/2002" Address="ElNozha" Amount="2000"></HistoryItem>
+                <HistoryItem Date="24/2/2002" Address="ElNozha" Amount="2000"></HistoryItem>
+            </div>
         </>
     );
 }
