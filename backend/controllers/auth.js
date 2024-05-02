e762266d4935fb7e4e9336e9c6dcaa539f0fc68e
Zeyad Waleed Amin <ziadwalid20035@gmail.com>
2024-05-02 16:19:49 +0300

    add token generator function and make signup return token

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,10 +1,10 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
 
-const { findByEmailAndType } = require("../utilities/find");
-const { createUser } = require("../utilities/create");
-const { checkValidation } = require("../utilities/check");
+const { validationResult } = require('express-validator');
+const tokenGenerator = require('../utilities/token_generator');
+const { findByEmailAndType } = require('../utilities/find');
+const { createUser } = require('../utilities/create');
+const { checkValidation } = require('../utilities/check');
 
 exports.signup = async (req, res, next) => {
   const errors = validationResult(req);
@@ -13,9 +13,12 @@ exports.signup = async (req, res, next) => {
     const hashedPassword = await bcrypt.hash(req.body.password, 12);
     const result = await createUser(req, hashedPassword);
 
+    const token = tokenGenerator(result.email, result._id, req.body.userType);
+
     res.status(201).json({
-      message: "User Created Successfuly.",
+      message: 'User Created Successfuly.',
       user: result._doc,
+      token,
     });
   } catch (err) {
     if (!err.status) {
@@ -39,20 +42,15 @@ exports.login = async (req, res, next) => {
     );
     if (!passwordComparisonResult) {
       const error = new Error(
-        "Password Is Invalid. Please Enter A Valid Password."
+        'Password Is Invalid. Please Enter A Valid Password.'
       );
       error.status = 422;
       throw error;
     }
-
-    const token = jwt.sign(
-      { email, userId: user._id.toString(), userType },
-      "ThisIsASecretKeyYouShouldNotShareItWithAnyOne",
-      { expiresIn: "1h" }
-    );
+    const token = tokenGenerator(email, user._id, userType);
 
     res.status(200).json({
-      message: "User Logged In Successfuly.",
+      message: 'User Logged In Successfuly.',
       token,
       tokenExpiryDate: Date.now() + 3600000,
     });
