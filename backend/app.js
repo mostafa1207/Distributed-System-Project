ac0fab672e48773dd1300de8e746de01177ea3c1
Zeyad Waleed Amin <ziadwalid20035@gmail.com>
2024-09-14 18:48:09 +0300

    fix: dotenv bug

--- backend/app.js
+++ backend/app.js
@@ -1,38 +1,35 @@
-const path = require("path");
+const path = require('path');
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
 
-const express = require("express");
-const mongoose = require("mongoose");
-const multer = require("multer");
-
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user");
-const sellerRoutes = require("./routes/seller");
-const customerRoutes = require("./routes/customer");
-const guestRoutes = require("./routes/guest");
-const cors = require("./middlewares/CORS");
-const errorHandler = require("./middlewares/error-handler");
-const resourceNotFoundHandler = require("./middlewares/notfound");
-const { storage } = require("./utilities/image-upload");
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const sellerRoutes = require('./routes/seller');
+const customerRoutes = require('./routes/customer');
+const guestRoutes = require('./routes/guest');
+const cors = require('./middlewares/CORS');
+const errorHandler = require('./middlewares/error-handler');
+const resourceNotFoundHandler = require('./middlewares/notfound');
+const { storage } = require('./utilities/image-upload');
 
 const app = express();
+dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(multer({ storage }).single("image"));
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(multer({ storage }).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(cors);
-app.use("/auth", authRoutes);
-app.use("/seller", sellerRoutes);
-app.use("/customer", customerRoutes);
-app.use("/guest", guestRoutes);
+app.use('/auth', authRoutes);
+app.use('/seller', sellerRoutes);
+app.use('/customer', customerRoutes);
+app.use('/guest', guestRoutes);
 app.use(userRoutes);
 app.use(resourceNotFoundHandler);
 app.use(errorHandler);
-mongoose
-  .connect(
-    "mongodb+srv://zeyad:6EwDCVKSbrowMQ7m@cluster0.jitwnwb.mongodb.net/distributed?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then(() => {
-    app.listen(3000, () => {
-      console.log("server started");
-    });
+mongoose.connect(process.env.DATABASE_URL).then(() => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('server started');
   });
+});
--- backend/utilities/token_generator.js
+++ backend/utilities/token_generator.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (email, id, userType) => {
   const token = jwt.sign(
     { email, userId: id.toString(), userType },
-    'ThisIsASecretKeyYouShouldNotShareItWithAnyOne',
+    process.env.SECRET,
     { expiresIn: '1h' }
   );
   return token;
